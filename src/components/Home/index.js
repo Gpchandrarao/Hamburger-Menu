199b936548a48e6ab619f0237c1e0888862d9ba4
gpchandrarao <gandikotapoornachandrarao@gmail.com>
2023-04-29 07:19:23 +0000

    Hamburger Menu

---
+++ src/components/Home/index.js
@@ -0,0 +1,28 @@
+// Write your code here
+
+// https://assets.ccbp.in/frontend/react-js/home-sm-img.png alt should be home
+// https://assets.ccbp.in/frontend/react-js/home-lg-img.png alt should be home
+
+import Header from '../Header'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <Header />
+    <div className="home-image-container">
+      <img
+        className="mobile-home-image"
+        src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+        alt="home"
+      />
+      <img
+        className="desktop-home-image"
+        src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+        alt="home"
+      />
+    </div>
+  </div>
+)
+
+export default Home
